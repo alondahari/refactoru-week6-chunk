685b4e54989a53d54549058d77bb73aa7eec3b56
Alon Dahari <climbinghobo@gmail.com>
2015-02-17 09:15:31 -0700

    fixed chunk

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var chunk = function(input, num, arr){
 	arr = arr || [];
-	var chunkSize;
+	var chunkSize = Math.ceil(input.length / num);;
 
-	while (input.length) {
-		chunkSize = Math.ceil(input / num--);
+	while (input.length >= chunkSize) {
+		chunkSize = Math.ceil(input.length / num--);
 		arr.push(input.splice(0, chunkSize));
 	}
 
--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,7 @@
 describe('chunk', function() {
   it('should split arrays into chunks', function() {
     expect(chunk([1,2,3,4,5,6,7,8,9,10], 2)).toEqual([ [1,2,3,4,5], [6,7,8,9,10] ]);
+    expect(chunk([1,2,3,4,5,6,7,8,9,10], 3)).toEqual([ [1,2,3,4], [5,6,7], [8,9,10] ]);
+    expect(chunk([1,2,3,4,5,6,7,8,9,10], 7)).toEqual([ [1,2], [3,4], [5,6], [7], [8], [9], [10] ]);
   });
 });
